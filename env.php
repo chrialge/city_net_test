c45f9df4828bc8f37cbe9c710ff0237e9624937d
chrialge <chrialge@gmail.com>
2025-05-01 09:10:53 +0200

    add change name database

--- env.php
+++ env.php
@@ -2,5 +2,5 @@
 define('DB_SERVERNAME', 'sandbox.citynetgroup.com');
 define('DB_USERNAME', 'sandbox_algieri');
 define('DB_PASSWORD', 'a4)sN9L%3x%R');
-define('DB_NAME', 'example_db');
+define('DB_NAME', 'sandbox_algieri');
 define('DB_PORT', '3306');
